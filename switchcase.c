023f82ae11e8b96ae1aa79164f7fc5e809d06af0
ayushharsh <60137954+ayushharsh@users.noreply.github.com>
2020-01-21 04:04:07 -0800

    Update switchcase.c

--- switchcase.c
+++ switchcase.c
@@ -1,49 +1,34 @@
 #include<stdio.h>
-#include<malloc.h>
-struct node{
-	int value;
-	struct node *next;
-};
-struct node *start=NULL;
-
-struct node *create(struct node*){
-	int data;
-	struct node *ptr,*pre;
-	ptr = (struct node*)malloc(sizeof(struct node));
-	printf("Enter the value");
-	scanf("%d",&data);
-	ptr->value = data;
-	if(start == NULL){
-		ptr->next=NULL;
-		start=ptr;
-	}else{
-		ptr->next=start;
-		start=ptr;
-	}
-	return start;
-}
-struct node *pop(struct node*){
-	if(start==NULL){
-		printf("Stack is Empty\n");
-	}else{
-	
-	printf("pop value %d",start->value);
-	start=start->next;}
-}
-int main(){
-	printf("Enter the option");
-	int option=0;
-	printf("Enter 1 to push the data\n");
-	printf("Enter 2 to pop the data\n");
-	printf("Enter 0 to quit ");
-	scanf("%d",&option);
-	while(option!=0){
-		switch(option){
-			case 1:create(start);break;
-			case 2:pop(start);break;
-			default : printf("Invalid option\n");break;
-		}
-		printf("Enter the option\n");
-		scanf("%d",&option);
-	}
+int main()
+{
+    int week;
+    printf("enter week number (1-7):");
+    scanf("%d",&week);
+    switch(week)
+    {
+    case 1:
+        printf("monday");
+        break;
+    case 2:
+        printf("tuesday");
+        break;
+    case 3:
+        printf("wednesday");
+        break;
+    case 4:
+        printf("thursday");
+        break;
+    case 5:
+        printf("friday");
+        break;
+    case 6:
+        printf("saturday");
+        break;
+    case 7:
+        printf("sunday");
+        break;
+        default:
+        printf("invalid input! please enter week number between 1_7.");
+    }
+    return 0;
 }
